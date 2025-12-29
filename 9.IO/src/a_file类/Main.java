571418aee9a24b8b4c62e651dbadc851a938cbf5
zzy <zy_npu@163.com>
2025-12-29 20:10:55 +0800

    2025.12.29 IO

---
+++ 9.IO/src/a_file类/Main.java
@@ -0,0 +1,30 @@
+package a_file类;
+
+/**
+ * File类位于java.io包下
+ * File()可以将文件(或目录)转化为一个实例对象,这个文件可以先不存在，操作过后自动创建
+ * 核心API:
+ * 1)createNewFile() 创建文件
+ * 2)mkdirs() 创建文件夹/目录
+ */
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        //File()创建对象
+        //方式1:
+        File file1 = new File("C:\\Users\\ZY\\Desktop");
+        //方式2:
+        File file2 = new File("C:\\Users\\ZY\\", "Desktop");
+        //方式3:
+        File file3 = new File("C:\\Users\\ZY\\");
+        File file4 = new File(file3,"Desktop");
+
+        //创建文件
+        File file = new File("C:\\Users\\ZY\\Desktop\\IO\\Test.java");
+        System.out.println(file.createNewFile());
+        //创建文件夹/目录
+        File file_ = new File("C:\\Users\\ZY\\Desktop\\IO\\io\\io\\");
+        System.out.println(file_.mkdirs());
+    }
+}
